46efc5a98f0b7f71d05ccb39c421bca929832973
= <=>
2025-04-28 17:52:12 -0400

    actividad7_1

--- controllers/post_create.php
+++ controllers/post_create.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     $user_id =  $_POST["user_id"];
     $title = $_POST["title"];
     $content = $_POST["content"];
-    
+    //dddd
     $post = new Post();
     $post->create($title,$content,$user_id);
     header('Location: user_index.php');
